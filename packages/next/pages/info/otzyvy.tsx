ba15d138045a2ae140c3c01875f4336dd8758fe1
alex postnikov <postnikov@gmail.com>
2021-03-04 15:34:56 +0200

    tubanext

---
+++ packages/next/pages/info/otzyvy.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { client } from '../../lib/api/api-client';
+import { getStore } from '@pdeals/next/stores/initStore';
+import NormalLayout from '@pdeals/next/components/layouts/NormalLayout';
+import { inject, observer } from 'mobx-react';
+import * as i18n from '@pdeals/next/utils/i18n';
+import Snippet from '@pdeals/next/components/Snippet/Snippet';
+
+export async function getServerSideProps(context) {
+  const alldata = await client().get('/open/alldata');
+  return {
+    props: { alldata }, // will be passed to the page component as props
+  };
+}
+
+const CheckoutPage: React.FunctionComponent<any> = ({ uiStore, alldata }) => {
+  // const { query } = useRouter();
+  uiStore.setAllData(alldata);
+  return (
+    <NormalLayout>
+      <div className="d-flex w-100">
+        <div>
+          <Snippet code={'otzyvy'} />
+        </div>
+      </div>
+    </NormalLayout>
+  );
+};
+
+export default inject('uiStore')(observer(CheckoutPage));
