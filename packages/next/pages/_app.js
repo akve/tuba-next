afb126b7882ada8582b06001e03ae566bfb5a8af
alex postnikov <postnikov@gmail.com>
2021-04-06 19:18:28 +0300

    yandex metrika

--- packages/next/pages/_app.js
+++ packages/next/pages/_app.js
@@ -117,6 +117,21 @@ export default class MyApp extends App {
             fbq('init', '366138666843162');
             fbq('track', 'PageView');`}
           </Safe.script>
+          <Safe.script>
+            {`
+               (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+               m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            
+               ym(75263086, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+                    ecommerce:"dataLayer"
+               });
+            `}
+          </Safe.script>
           <noscript>
             <img
               height="1"
