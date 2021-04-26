8e14ce316ef07b4133f7fc751b76c1b78435e3e5
alex postnikov <postnikov@gmail.com>
2021-04-27 00:35:28 +0300

    fb chat plugin

--- packages/next/pages/_document.tsx
+++ packages/next/pages/_document.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript, Html } from 'next/document';
+import Safe from 'react-safe';
 
 class MyDocument extends Document {
   render() {
@@ -14,6 +15,24 @@ class MyDocument extends Document {
           <link rel="stylesheet" as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" />
         </Head>
         <body>
+          <div id="fb-root"></div>
+          <Safe.script>
+            {`window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+          (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/ru_RU/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));`}
+          </Safe.script>
+          <div className="fb-customerchat" attribution="setup_tool" page_id="785826554851503"></div>
           <div id="page-transition"></div>
           <Main />
           <NextScript />
