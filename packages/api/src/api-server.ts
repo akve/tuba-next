6a5d9582b94095f3325a71c9a9a70bcad113bda7
alex postnikov <postnikov@gmail.com>
2021-05-11 20:16:13 +0300

    cors

--- packages/api/src/api-server.ts
+++ packages/api/src/api-server.ts
@@ -108,7 +108,23 @@ export class ApiServer {
     );
     this.app.use('/v1/uploads', express.static(config.uploadsPath));
     this.app.use('/uploads', express.static(config.uploadsPath));
+    this.app.use(function (req, res, next) {
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
 
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+
+      // Pass to next layer of middleware
+      next();
+    });
     this.app.use(cors());
     this.app.use(
       morgan('combined', {
