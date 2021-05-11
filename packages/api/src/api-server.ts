3a93f00f941320e7a3d6738e87cbacdb85109147
alex postnikov <postnikov@gmail.com>
2021-05-11 20:17:34 +0300

    cors

--- packages/api/src/api-server.ts
+++ packages/api/src/api-server.ts
@@ -120,7 +120,7 @@ export class ApiServer {
 
       // Set to true if you need the website to include cookies in the requests sent
       // to the API (e.g. in case you use sessions)
-      res.setHeader('Access-Control-Allow-Credentials', true);
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
 
       // Pass to next layer of middleware
       next();
