9239db30567a7121a5a874f87c464b8ca960c492
alex postnikov <postnikov@gmail.com>
2022-12-27 14:16:22 +0200

    gtag

--- packages/next/pages/_app.js
+++ packages/next/pages/_app.js
@@ -131,6 +131,15 @@ export default class MyApp extends App {
 
             gtag('config', 'UA-84542153-1');`}
           </Safe.script>
+          <Safe.script>
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M6F26DG')
+            `}
+          </Safe.script>
 
           {/*<Safe.script>
             {`!function(f,b,e,v,n,t,s)
