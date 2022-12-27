aec20cf7de2073f4e20bda6d1c5a97f6deecdc01
alex postnikov <postnikov@gmail.com>
2022-12-27 14:25:56 +0200

    no-gtag...

--- packages/next/pages/_document.tsx
+++ packages/next/pages/_document.tsx
@@ -45,7 +45,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6F26DG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <div id="page-transition"></div>
           <Main />
           <NextScript />
