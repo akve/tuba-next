0558d0463db9dd7e8c7e9e8e18d1446201693711
Alex Postnikov <postnikov@gmail.com>
2023-08-24 15:44:36 +0300

    unique ids

--- packages/next/components/Product/ProductContent.tsx
+++ packages/next/components/Product/ProductContent.tsx
@@ -231,16 +231,16 @@ function ProductContent(props: IProps) {
             {i18n.t('[E:Buy][R:Купить][U:Купити]')}
           </button>
           {!contactOpen && (
-            <a className="btn btn-secondary" onClick={()=>setContactOpen(true)}>
+            <a className="btn btn-secondary" id="direct-btn" onClick={()=>setContactOpen(true)}>
               <img src={"/assets/img/quote.svg"} height={20}/><i className="fas fa-question-circle"/>&nbsp;{i18n.t('[E:Contact directly][R:Insta][U:Написати в дірект]')}
             </a>
           )}
           {contactOpen && (
             <>
-              <a className="btn btn-secondary" href={"https://www.instagram.com/tubaiduba/reels/"}>
+              <a className="btn btn-secondary" id="direct-btn-insta" href={"https://www.instagram.com/tubaiduba/reels/"}>
                 <img src={"/assets/img/contacts-insta.png"} height={20}/>&nbsp;{i18n.t('[E:Contact on Instagram][R:Insta][U:Написати в Інстаграм]')}
               </a>
-              <a className="btn btn-secondary" href={"https://www.facebook.com/tubaiduba"}>
+              <a className="btn btn-secondary" id="direct-btn-fb" href={"https://www.facebook.com/tubaiduba"}>
                 <img src={"/assets/img/contacts-fb.png"} height={20}/>&nbsp;{i18n.t('[E:Contact on Facebook][R:FB][U:Написати в Facebook]')}
               </a>
             </>
